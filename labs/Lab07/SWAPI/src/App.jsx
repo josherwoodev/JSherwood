5d89d131d9fb9c5876c0fccd79b058d7faa6896b
Josh Sherwood <joshua.sherwood.mil@swf.army.mil>
2025-03-17 15:37:37 -0500

    Begun implepenting buttons. No functionality yet.

--- labs/Lab07/SWAPI/src/App.jsx
+++ labs/Lab07/SWAPI/src/App.jsx
@@ -3,10 +3,25 @@ import Table from "./components/Table.jsx";
 
 export default function App() {
     const [rows, setRows] = useState([]);
+    const [displaying, setDisplaying] = useState(false);
+    const STYLE = {
+        display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', width: '100vw', height: '100vh', background: 'url("./assets/outerSpaceWars.jpg") no-repeat center bottom fixed'
+    };
 
     useEffect(() => {
-        setRows([{name:'Joe',height:'5\'8"',hair_color:'black',gender:'male'}]);
-    }, []);
+        setRows([{name: 'Joe', height: '5\'8"', hair_color: 'black', gender: 'male'}]);
+    }, [displaying]);
 
-    return (<Table data={rows} />);
+    return (<div style={STYLE}>
+            <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}><h1>SWAPI</h1><h2>Star Wars API</h2>
+                <div>
+                    <button>Submit</button>
+                    <button onClick={() => {
+                        setDisplaying(!displaying);
+                    }}>Reset
+                    </button>
+                </div>
+            </div>
+            {displaying ? <Table data={rows}/> : null}
+        </div>);
 }
