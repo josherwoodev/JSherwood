efa5d005d8bb683f95801f61c8b6eeabcd2fa830
Josh Sherwood <joshua.sherwood.mil@swf.army.mil>
2025-03-17 14:52:21 -0500

    Initial Lab 07.

---
+++ labs/Lab07/SWAPI/src/App.jsx
@@ -0,0 +1,11 @@
+import {useState} from 'react'
+
+export default function App() {
+    const [count, setCount] = useState(0)
+
+    return (<>
+        <p className="read-the-docs">
+            Click on the Vite and React logos to learn more
+        </p>
+    </>);
+}
