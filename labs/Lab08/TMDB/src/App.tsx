2402e6159e51c54c41e403185ccda1dd66fc5fd9
Josh Sherwood <joshua.sherwood.mil@swf.army.mil>
2025-03-18 13:24:54 -0500

    Implemented MUI.

--- labs/Lab08/TMDB/src/App.tsx
+++ labs/Lab08/TMDB/src/App.tsx
@@ -1,9 +1,12 @@
+import {Button} from "@mui/material";
+
 export default function App() {
 
     return (
         <>
             <div>
                 <p>{import.meta.env.VITE_TMDB_API_TOKEN}</p>
+                <Button variant="outlined" color="primary">MUI Button</Button>
             </div>
         </>
     );
