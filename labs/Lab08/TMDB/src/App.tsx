54246cef2b1ce5f72336e2d8fcb95ee6184db444
Josh Sherwood <joshua.sherwood.mil@swf.army.mil>
2025-03-18 13:13:49 -0500

    Implemented .env.

--- labs/Lab08/TMDB/src/App.tsx
+++ labs/Lab08/TMDB/src/App.tsx
@@ -3,6 +3,7 @@ export default function App() {
     return (
         <>
             <div>
+                <p>{import.meta.env.VITE_TMDB_API_TOKEN}</p>
             </div>
         </>
     );
--- labs/Lab08/TMDB/src/vite-env.d.ts
+++ labs/Lab08/TMDB/src/vite-env.d.ts
@@ -1,4 +1,4 @@
 /// <reference types="vite/client" />
-export declare const ViteEnv: {
-    VITE_TMDB_API_TOKEN: "XXXX";
+interface ViteEnv {
+    readonly VITE_TMDB_API_TOKEN: string;
 }
