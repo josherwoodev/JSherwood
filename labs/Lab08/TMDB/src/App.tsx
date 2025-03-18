76eefa954615087a495b0e7a50a88409a50b921e
Josh Sherwood <joshua.sherwood.mil@swf.army.mil>
2025-03-18 13:34:37 -0500

    Initial page state tracker.

--- labs/Lab08/TMDB/src/App.tsx
+++ labs/Lab08/TMDB/src/App.tsx
@@ -1,13 +1,36 @@
 import {Button} from "@mui/material";
+import NavBar from "./components/NavBar.tsx";
+import {useState} from "react";
+import {PAGES} from "./types.ts";
 
 export default function App() {
+    const [page, setPage] = useState<PAGES>(PAGES.INDEX);
 
     return (
         <>
+            <NavBar></NavBar>
             <div>
                 <p>{import.meta.env.VITE_TMDB_API_TOKEN}</p>
-                <Button variant="outlined" color="primary">MUI Button</Button>
+                <Button variant="outlined" color="primary" onClick={() => setPage(page + 1)}>MUI Button</Button>
             </div>
+            {getPage(page)}
         </>
     );
+}
+
+function getPage(page: PAGES) {
+    let val: string;
+    switch (page) {
+        case PAGES.INDEX:
+            val = 'index';
+            break;
+        case PAGES.RESULTS:
+            val = 'results';
+            break;
+        case PAGES.ERROR:
+        default:
+            val = 'error';
+            break;
+    }
+    return (<>{val}</>);
 }
---
+++ labs/Lab08/TMDB/src/types.ts
@@ -0,0 +1,5 @@
+export enum PAGES {
+    INDEX,
+    RESULTS,
+    ERROR
+}
