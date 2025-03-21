5839a1f3b893ad237318b1410b78d99b597c503f
Josh Sherwood <joshua.sherwood.mil@swf.army.mil>
2025-03-21 10:03:34 -0500

    Implemented api call for now playing.

--- labs/Lab08/TMDB/src/components/Card.tsx
+++ labs/Lab08/TMDB/src/components/Card.tsx
@@ -1,3 +1,3 @@
-export default function Card() {
-    return (<></>);
+export default function Card(props:any) {
+    return (<p>{props.data.title}  {props.data.poster_path}  {props.data.overview}</p>);
 }
---
+++ labs/Lab08/TMDB/src/components/NowPlaying.tsx
@@ -0,0 +1,13 @@
+import {useEffect, useState} from "react";
+import Card from "./Card.tsx";
+import {reqNowPlaying} from "../helpers.ts";
+
+export default function NowPlaying() {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(()=>{reqNowPlaying().then((res)=>res.data).then((data)=>setMovies(data.results)).catch((err)=>console.error(err));});
+
+    return (<>
+        {movies.map((movie, index) => (<Card key={index} data={movie}/>))}
+    </>);
+}
