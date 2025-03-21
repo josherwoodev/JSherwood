1075b9615ae4fb01a9c585b332a7ccda33d77c63
Josh Sherwood <joshua.sherwood.mil@swf.army.mil>
2025-03-21 10:00:33 -0500

    Implemented image url builder.

--- labs/Lab08/TMDB/src/helpers.ts
+++ labs/Lab08/TMDB/src/helpers.ts
@@ -1,9 +1,17 @@
 import axios from "axios";
 
+const BASE_URL = "https://image.tmdb.org/t/p";
+const SIZE_342 = "w342";
+// const SIZE_500 = "w500";
+
 export function reqNowPlaying() {
     return axios.get("https://api.themoviedb.org/3/movie/now_playing", {headers:{Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_TOKEN}`}});
 }
 
 export function reqSearch(str: string) {
     return axios.get(`https://api.themoviedb.org/3/search/movie?query=${encodeURI(str)}`, {headers:{Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_TOKEN}`}});
+}
+
+export function buildImgRef(url: string): string {
+    return `${BASE_URL}/${SIZE_342}/${url.split(".")[-1] === "svg" ? url.replace(".svg", ".png") : url}`;
 }
