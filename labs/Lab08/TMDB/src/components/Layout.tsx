774a8a98d3871b519883defb838cb8ac2a87b8e6
Josh Sherwood <joshua.sherwood.mil@swf.army.mil>
2025-03-24 13:40:13 -0500

    Added background image.

--- labs/Lab08/TMDB/src/components/Layout.tsx
+++ labs/Lab08/TMDB/src/components/Layout.tsx
@@ -3,11 +3,13 @@ import NavBar from "./NavBar.tsx";
 import {Outlet} from "react-router-dom";
 import {ThemeProvider} from "@emotion/react";
 import {theme} from "./ui/Theme.tsx";
+import bgImage from "../assets/theaterBg.jpg";
 
 export default function Layout() {
 
     return (
         <ThemeProvider theme={theme}>
+            <div style={{position: "fixed", top: 0, left: 0, right: 0, bottom: 0, background: `#2228 url('${bgImage}') center center no-repeat`, backgroundBlendMode: 'darken', backgroundAttachment: 'fixed', backgroundSize: 'cover'}} />
             <NavBar links={[{text: "Home", href: "/"}, {text: "Now Playing", href: "/now-playing"}]} search/>
             <div style={{flexGrow: 1, width: '100%', overflowY: 'scroll'}}>
                 <Button variant="outlined" color="primary">MUI Button</Button>
