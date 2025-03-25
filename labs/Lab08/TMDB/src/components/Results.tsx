f797c312c1e0a9a940df3d7c9bd3e661406507d7
Josh Sherwood <joshua.sherwood.mil@swf.army.mil>
2025-03-25 12:46:27 -0500

    Implemented search results page.

--- labs/Lab08/TMDB/src/components/Results.tsx
+++ labs/Lab08/TMDB/src/components/Results.tsx
@@ -1,6 +1,18 @@
 import {useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import {reqSearch} from "../helpers.ts";
+import {MovieRes} from "../types.ts";
+import Card from "./Card.tsx";
 
 export default function Results() {
     const {searchTerm} = useParams();
-    return (<>Searched: {searchTerm}</>);
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        reqSearch(searchTerm).then((res) => res.data).then((data) => setMovies(data.results)).catch((err) => console.error(err));
+    }, [searchTerm]);
+
+    return (<div style={{display: "flex", flexDirection: "row", alignContent: "start", justifyContent: "space-around", flexWrap: "wrap", padding: 10}}>
+        {movies.map((movie: MovieRes, index) => (<Card key={index} {...movie}/>))}
+    </div>);
 }
