0d1d0afdad20068a5539a74db7a9a0598f4e758d
Josh Sherwood <joshua.sherwood.mil@swf.army.mil>
2025-03-25 10:44:52 -0500

    Built movie cards. Implemented cards in now playing.

--- labs/Lab08/TMDB/src/components/Card.tsx
+++ labs/Lab08/TMDB/src/components/Card.tsx
@@ -1,3 +1,34 @@
-export default function Card(props:any) {
-    return (<p>{props.data.title}  {props.data.poster_path}  {props.data.overview}</p>);
+import {buildImgRef, SIZE_185, SIZE_342, useWindowDimensions} from "../helpers.ts";
+import {CSSProperties, useEffect, useState} from "react";
+
+export default function Card(props: any) {
+    const {width} = useWindowDimensions();
+    const [minified, setMinified] = useState(width < 1600);
+    const STYLE: CSSProperties = {
+        display: 'flex',
+        flexDirection: 'column',
+        placeItems: 'center',
+        backgroundColor: "#334",
+        padding: 10,
+        margin: 10,
+        boxShadow: '#aaa 3px 3px 5px'
+    };
+    const CONSTRAINED: CSSProperties = {
+        maxWidth: minified ? SIZE_185 + 20 : SIZE_342,
+        marginTop: 5,
+        marginBottom: 5
+    };
+
+    useEffect(() => {
+        setMinified(width < 1600);
+    }, [width]);
+
+    return (
+        <div style={STYLE}>
+            <img src={buildImgRef(props.data.poster_path, minified)} style={{width: minified ? SIZE_185 : SIZE_342}}/>
+            <h4 style={CONSTRAINED}>{props.data.title}</h4>
+            <p style={CONSTRAINED}>{props.data.overview}</p>
+            <p style={CONSTRAINED}>{props.data.vote_average} / 10 ({props.data.vote_count}) {width}</p>
+        </div>
+    );
 }
--- labs/Lab08/TMDB/src/components/NowPlaying.tsx
+++ labs/Lab08/TMDB/src/components/NowPlaying.tsx
@@ -5,9 +5,11 @@ import {reqNowPlaying} from "../helpers.ts";
 export default function NowPlaying() {
     const [movies, setMovies] = useState([]);
 
-    useEffect(()=>{reqNowPlaying().then((res)=>res.data).then((data)=>setMovies(data.results)).catch((err)=>console.error(err));},[]);
+    useEffect(() => {
+        reqNowPlaying().then((res) => res.data).then((data) => setMovies(data.results)).catch((err) => console.error(err));
+    }, []);
 
-    return (<>
+    return (<div style={{display: "flex", flexDirection: "row", alignContent: "start", justifyContent: "space-around", flexWrap: "wrap", padding: 10}}>
         {movies.map((movie, index) => (<Card key={index} data={movie}/>))}
-    </>);
+    </div>);
 }
