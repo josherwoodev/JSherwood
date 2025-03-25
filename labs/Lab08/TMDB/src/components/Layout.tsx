05b503045fce2471f66021582b9fae95f8d68b36
Josh Sherwood <joshua.sherwood.mil@swf.army.mil>
2025-03-25 13:27:29 -0500

    Finalized layout and navbar design.

--- labs/Lab08/TMDB/src/components/Layout.tsx
+++ labs/Lab08/TMDB/src/components/Layout.tsx
@@ -1,21 +1,38 @@
-import {Button} from "@mui/material";
 import NavBar from "./NavBar.tsx";
 import {Outlet} from "react-router-dom";
 import {ThemeProvider} from "@emotion/react";
 import {theme} from "./ui/Theme.tsx";
 import bgImage from "../assets/theaterBg.jpg";
+import {CSSProperties} from "react";
 
 export default function Layout() {
+    const STYLE: CSSProperties = {
+        position: "fixed",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        background: `#2228 url('${bgImage}') center center no-repeat`,
+        backgroundBlendMode: 'darken',
+        backgroundAttachment: 'fixed',
+        backgroundSize: 'cover',
+        zIndex: -1
+    };
+    const LINKS = [
+        {text: "Home", href: "/"},
+        {text: "Now Playing", href: "/now-playing"}
+    ];
 
     return (
         <ThemeProvider theme={theme}>
-            <div style={{position: "fixed", top: 0, left: 0, right: 0, bottom: 0, background: `#2228 url('${bgImage}') center center no-repeat`, backgroundBlendMode: 'darken', backgroundAttachment: 'fixed', backgroundSize: 'cover', zIndex: -1}}/>
-            <NavBar links={[{text: "Home", href: "/"}, {text: "Now Playing", href: "/now-playing"}]} search/>
+            <div style={STYLE}/>
+            <NavBar links={LINKS} search/>
             <div style={{flexGrow: 1, width: '100%', overflowY: 'scroll'}}>
-                <Button variant="outlined" color="primary">MUI Button</Button>
                 <Outlet/>
             </div>
-            <NavBar><p style={{width: '50%'}}><span className="inline"><b>TMDB</b></span> API display created as an assignment within Advanced Web Programming. Not for commercial use.</p></NavBar>
+            <NavBar><p style={{width: '50%'}}>
+                <span className="inline"><b>TMDB</b></span> API display created as an assignment within Advanced Web Programming. Not for commercial use.</p>
+            </NavBar>
         </ThemeProvider>
     );
 }
--- labs/Lab08/TMDB/src/components/NavBar.tsx
+++ labs/Lab08/TMDB/src/components/NavBar.tsx
@@ -11,14 +11,14 @@ export default function NavBar(props: any) {
     return (
         <AppBar position="sticky" color="primary"><Container maxWidth="lg">
             <Toolbar sx={{display: "flex", flexDirection: "row", placeContent: "start"}} disableGutters>
-                {props.links ? props.links.map((link: any, index: number) => (<Button key={index} href={link.href} color="inherit">{link.text}</Button>)) : null}
+                {props.links ? props.links.map((link: any, index: number) => (<Button key={index} href={link.href} color="secondary" variant="outlined" style={{margin:5}}>{link.text}</Button>)) : null}
                 <div style={{flexGrow: 1}}>{props.children}</div>
                 {props.search
                     ? <form action="#" onSubmit={e => {
                         e.preventDefault();
                         navigate("/search/" + searchText)
                     }}>
-                        <TextField color="info" label="Search ..." value={searchText} onChange={(e) => setSearchText(e.target.value)} variant="outlined" size="small" slotProps={{
+                        <TextField color="secondary" label="Search ..." value={searchText} onChange={(e) => setSearchText(e.target.value)} variant="outlined" size="small" slotProps={{
                             input: {
                                 endAdornment: (<InputAdornment position="end"><a style={A_STYLE} onClick={() => navigate("/search/" + searchText)}>
                                     <Search/></a></InputAdornment>)
