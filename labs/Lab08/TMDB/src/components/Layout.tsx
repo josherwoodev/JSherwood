85dc4016ca77e7fa50c9d55167221b46fd92e243
Josh Sherwood <joshua.sherwood.mil@swf.army.mil>
2025-03-25 10:43:21 -0500

    Fixed bg to not overwrite page content.

--- labs/Lab08/TMDB/src/components/Layout.tsx
+++ labs/Lab08/TMDB/src/components/Layout.tsx
@@ -9,7 +9,7 @@ export default function Layout() {
 
     return (
         <ThemeProvider theme={theme}>
-            <div style={{position: "fixed", top: 0, left: 0, right: 0, bottom: 0, background: `#2228 url('${bgImage}') center center no-repeat`, backgroundBlendMode: 'darken', backgroundAttachment: 'fixed', backgroundSize: 'cover'}}/>
+            <div style={{position: "fixed", top: 0, left: 0, right: 0, bottom: 0, background: `#2228 url('${bgImage}') center center no-repeat`, backgroundBlendMode: 'darken', backgroundAttachment: 'fixed', backgroundSize: 'cover', zIndex: -1}}/>
             <NavBar links={[{text: "Home", href: "/"}, {text: "Now Playing", href: "/now-playing"}]} search/>
             <div style={{flexGrow: 1, width: '100%', overflowY: 'scroll'}}>
                 <Button variant="outlined" color="primary">MUI Button</Button>
